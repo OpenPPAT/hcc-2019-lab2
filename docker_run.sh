24fa5d0555e14e49b7dd37f7f147e769e86208c1
samliu <c52647w@gmail.com>
2019-02-18 15:29:22 +0800

    add search textbook

---
+++ docker_run.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+# Setup the style of color
+RED='\033[0;31m'
+NC='\033[0m'
+
+# Find current directory and transfer it to container directory for Docker
+current_dir="$(pwd)"
+host_dir="/home/$USER/"
+container_dir="/hosthome/"
+goal_dir=${current_dir//$host_dir/$container_dir}
+echo "goal_dir: \"${goal_dir}\""
+
+Check the command 'nvidia-docker' is existing or not
+ret_code="$(command -v nvidia-docker)"
+if [ -z "$ret_code" ]
+then
+    printf "${RED}\"nvidia-docker\" is not found, so substitute docker. $NC\n"
+    docker run -it --rm -v /home/$USER:/hosthome -p 8888:8888 -w ${goal_dir} pytorch-jupyter 
+else
+    printf "Run \"nvidia-docker\"\n"
+    nvidia-docker run -it --rm -v /home/$USER:/hosthome -p 8888:8888 -w ${goal_dir} pytorch-jupyter 
+fi
+
+
+# # aa=$(jupyter notebook --no-browser \
+# #                 --allow-root \
+# #                 --ip="0.0.0.0" \
+# #                 --port="8888")
+# ip_list=$(ifconfig | awk '/inet addr/{print substr($2,6)}')
+# if 
+# aa=$(ls)
+
+# echo $aa |grep 'docker'
+
